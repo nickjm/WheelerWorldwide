7d8e353b6bf128f1ed844598d9a9fa0dee91653e
nickjm <nikk.matthews@gmail.com>
2018-09-07 12:58:50 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    function getRandomSize(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+    }
+
+    var allImages = [];
+
+    // for (var i = 0; i < 25; i++) {
+    //   // allImages += '<div class="cell"><img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty"></div>';
+    //   var kitty = $('<img src="https://placekitten.com/800/800" alt="pretty kitty">')
+    //   kitty.fluidbox();
+    //   var kitty_cage = $('<div class="cell"></div>');
+    //   kitty_cage.append(kitty);
+    //   $('#photos').append(kitty_cage);
+    // }
+});
